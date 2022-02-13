7aeefbaf442631d053293ce873bc4f7439720087
saidmsaif <saidmsaiff@gmail.com>
2022-02-13 17:58:29 +0600

    remove unused files and modify design

---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+    if(!function_exists('educare_theme_setup')) {
+        function educare_theme_setup() {
+            // Load Theme TextDomain
+            load_theme_textdomain('educare');
+    
+            // Theme Supports
+            add_theme_support('title-tag');
+            add_theme_support('description');
+            add_theme_support('widgets');
+            add_theme_support('post-thumbnails');
+            add_theme_support('custom-header');
+            add_theme_support('custom-logo');
+            add_theme_support('custom-background');
+            add_theme_support( 'automatic-feed-links' );
+    
+            add_theme_support(
+                'html5',
+                array(
+                    'comment-form',
+                    'comment-list',
+                    'gallery',
+                    'caption',
+                    'style',
+                    'script',
+                    'navigation-widgets',
+                )
+            );
+    
+            register_nav_menus(array(
+                'primary-menu'              =>  __('Primary Menu', 'craftnce'),
+            ));
+        }
+        add_action('after_setup_theme', 'educare_theme_setup');
+    }
+
+    function educare_assets_enqueue() {
+        // CSS Enqueue
+        wp_enqueue_style('gFont-one', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,800;1,300;1,400;1,600;1,700;1,800&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap');
+        wp_enqueue_style('gFont-two', 'https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:ital,wght@0,800;1,300;1,400;1,600;1,700;1,800&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap');
+        wp_enqueue_style( 'fontawesome-css', get_template_directory_uri().'/assets/css/all.css');
+        wp_enqueue_style( 'bootstrap-css', get_template_directory_uri().'/assets/css/bootstrap.css');
+        wp_enqueue_style( 'theme-style-css', get_template_directory_uri().'/assets/css/style.css');
+        wp_enqueue_style( 'responsive-css', get_template_directory_uri().'/assets/css/responsive.css');
+        wp_enqueue_style('main-css', get_stylesheet_uri());
+
+        // JS Enqueue
+        wp_enqueue_script( 'bootstrap-js', get_template_directory_uri().'/assets/js/bootstrap.bundle.js', array('jquery'), null, true );
+        wp_enqueue_script( 'main-js', get_template_directory_uri().'/assets/js/main.js', array('jquery'), null, true );
+    }
+    add_action( 'wp_enqueue_scripts', 'educare_assets_enqueue' );
---
+++ header.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php wp_head(); ?>
+</head>
+<body>
+    <!-- header-start  -->
+    <header>
+        <!-- top-header-strat  -->
+        <div class="main-container top-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="row">
+                        <div class="col-5 address">
+                            <div class="top-header-items">
+                                <span><i class="fas fa-home"></i></span>
+                                <p>Mirpur , Dhaka, Bangladesh</p>
+                            </div>
+                        </div>
+                        <div class="col-3 phone">
+                            <a href="#" class="top-header-phone top-header-items">
+                                <span><i class="fas fa-phone"></i></span>
+                                <p>39885677834390</p>
+                            </a>
+                        </div>
+                        <div class="col-2 gmail">
+                            <a href="#" class="top-header-mail top-header-items">
+                                <span><i class="far fa-envelope"></i></span>
+                                <p>Info@gmail.com</p>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="header-side-btn">
+                        <div class="d-flex justify-content-end">
+                            <div class="login-btn header-btn">
+                                <a class="login" href="#"><i class="fas fa-sign-in-alt"></i>Login</a>
+                            </div>
+                            <div class="register-btn header-btn">
+                                <a class="register" href="#"><i class="fas fa-key"></i>Register</a>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- top-header-close  -->
+        <!-- nav-bar-start  -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-white p-3 sticky-top">
+            <div class="container-xl sticky-top">
+                <a class="navbar-brand" href="index.html"><span class="logo-text-1">EDU</span><span class="logo-text-2">CARE</span></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span><i class="fas fa-bars"></i></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Courses</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Accademics</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Events</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Teachers</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Notice</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link admission-btn" href="#">Admission</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- nav-bar-close  -->
+    </header>
+    <!-- header-close  -->
