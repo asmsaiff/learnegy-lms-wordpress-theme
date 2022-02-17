78c6e294f3d5a91c098574173b34cc44d1d4517e
saidmsaif <saidmsaiff@gmail.com>
2022-02-17 12:32:04 +0600

    customizer config file edit

--- inc/config/customizer-config.php
+++ inc/config/customizer-config.php
@@ -2,24 +2,48 @@
     require_once get_template_directory() . '/lib/educare-customizer-panels.php';
 
     function pe_customize_register( $wp_customize ) {
-        $wp_customize->register_panel_type( 'EduCare_WP_Customize_Panel' );
-        $wp_customize->register_section_type( 'EduCare_WP_Customize_Section' );
+
+        // Has to be at the top
+        $wp_customize->register_panel_type( 'PE_WP_Customize_Panel' );
+        $wp_customize->register_section_type( 'PE_WP_Customize_Section' );
+
+        $educare_theme_options = new PE_WP_Customize_Panel( $wp_customize, 'educare_theme_options', array(
+            'title'             => 'EduCare Theme Options',
+            'priority'          => 1,
+        ));
+        $wp_customize->add_panel( $educare_theme_options );
 
         /**
-         *  ================================================
-         *  ================= Header Panel =================
-         *  ================================================
-         * 
-         *  You can customize those information from Customizer > Craftnce Options > Footer Section Panel.
-         *  You can also override the functions using child theme provided by Craftnce
+         * ================================================
+         * ================= Header Panel =================
+         * ================================================
          */
-        $educare_main_panel = new EduCare_WP_Customize_Panel( $wp_customize, 'educare_main_panel', array(
-            'title'             =>      'EduCare Theme Options',
-            'priority'          =>      1,
+        $educare_header = new PE_WP_Customize_Panel( $wp_customize, 'educare_header', array(
+            'title'             => 'Header',
+            'panel'             => 'educare_theme_options',
         ));
-        $wp_customize->add_panel( $educare_main_panel );
+        $wp_customize->add_panel( $educare_header );
 
-        require_once get_theme_file_path('/inc/option-panel/common/op-header.php');
+        require_once get_template_directory() . '/inc/option-panel/common/op-header.php';
+
+        // // Add example section and controls to the middle (second) panel
+        // $wp_customize->add_section( 'pe_section_2', array(
+        //     'title'             => 'Section 2 Test',
+        //     'panel'             => 'educare_header',
+        //     'priority'          => 2,
+        // ));
+
+        // $wp_customize->add_setting( 'pe_test_2', array(
+        //     'default'           => 'default value here',
+        //     'sanitize_callback' => 'wp_kses_post',
+        //     'transport'         => 'postMessage',
+        // ));
+
+        // $wp_customize->add_control( 'pe_test_2', array(
+        //     'type'              => 'text',
+        //     'label'             => 'Some text control 2',
+        //     'section'           => 'pe_section_2',
+        // ));
     }
 
     add_action( 'customize_register', 'pe_customize_register' );
