09b8cf83ad6e3e4c9d1a6fb0f598e582c89fc6a6
saidmsaif <saidmsaiff@gmail.com>
2022-02-19 18:22:25 +0600

    add customizer repeater test field

--- inc/option-panel/common/op-header.php
+++ inc/option-panel/common/op-header.php
@@ -16,3 +16,14 @@
         'label' => 'Some text control 2',
         'section' => 'pe_section_2',
     ));
+
+    $wp_customize->add_setting( 'educare_home_item_settings', array(
+        'sanitize_callback' => 'customizer_repeater_sanitize'
+    ));
+    $wp_customize->add_control( new Customizer_Repeater( $wp_customize, 'educare_home_item_ctrl', array(
+        'label'                                             => esc_html__('Accordion Item','educare'),
+        'section'                                           => 'pe_section_2',
+        'settings'                                          =>  'educare_home_item_settings',
+        'customizer_repeater_title_control'                 => true,
+        'customizer_repeater_text_control'                  => true,
+    )));
