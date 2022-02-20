cfa79bec6bf76c21ec2013d167703d79e55c8bf5
saidmsaif <saidmsaiff@gmail.com>
2022-02-20 11:06:31 +0600

    dynamic topbar

--- inc/option-panel/op-header.php
+++ inc/option-panel/op-header.php
@@ -14,8 +14,88 @@
         }
     ));
     $wp_customize->add_control('educare_show_topbar_ctrl', array(
-        'label'             =>  __('Show topbar', 'craftnce'),
+        'label'             =>  __('Show topbar', 'educare'),
         'section'           =>  'educare_topbar',
         'settings'          =>  'educare_show_topbar_settings',
         'type'              =>  'checkbox'
+    ));
+
+    $wp_customize->add_setting('educare_topbar_left_text_settings', array(
+        'capability'        => 'edit_theme_options',
+        'transport'         => 'refresh',
+        'type'              => 'theme_mod',
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    $wp_customize->add_control('educare_topbar_left_text_ctrl', array(
+        'label'             =>  __('Left Text', 'educare'),
+        'section'           =>  'educare_topbar',
+        'settings'          =>  'educare_topbar_left_text_settings',
+        'type'              =>  'text'
+    ));
+
+    $wp_customize->add_setting('educare_topbar_tel_settings', array(
+        'capability'        => 'edit_theme_options',
+        'transport'         => 'refresh',
+        'type'              => 'theme_mod',
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    $wp_customize->add_control('educare_topbar_tel_ctrl', array(
+        'label'             =>  __('Phone Number', 'educare'),
+        'section'           =>  'educare_topbar',
+        'settings'          =>  'educare_topbar_tel_settings',
+        'type'              =>  'text'
+    ));
+
+    $wp_customize->add_setting('educare_topbar_email_settings', array(
+        'capability'        => 'edit_theme_options',
+        'transport'         => 'refresh',
+        'type'              => 'theme_mod',
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    $wp_customize->add_control('educare_topbar_email_ctrl', array(
+        'label'             =>  __('Email Address', 'educare'),
+        'section'           =>  'educare_topbar',
+        'settings'          =>  'educare_topbar_email_settings',
+        'type'              =>  'text'
+    ));
+
+    $wp_customize->add_setting('educare_show_topbar_right_btn_settings', array(
+        'capability'        => 'edit_theme_options',
+        'transport'         => 'refresh',
+        'type'              => 'theme_mod',
+        'sanitize_callback' => function( $input ) {
+            return ( ( isset( $input ) && true == $input ) ? true : false );
+        }
+    ));
+    $wp_customize->add_control('educare_show_topbar_right_btn_ctrl', array(
+        'label'             =>  __('Show right side button', 'educare'),
+        'section'           =>  'educare_topbar',
+        'settings'          =>  'educare_show_topbar_right_btn_settings',
+        'type'              =>  'checkbox'
+    ));
+
+    $wp_customize->add_setting('educare_show_topbar_right_btn_label_settings', array(
+        'capability'        => 'edit_theme_options',
+        'transport'         => 'refresh',
+        'type'              => 'theme_mod',
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    $wp_customize->add_control('educare_show_topbar_right_btn_label_ctrl', array(
+        'label'             =>  __('Button Label', 'educare'),
+        'section'           =>  'educare_topbar',
+        'settings'          =>  'educare_show_topbar_right_btn_label_settings',
+        'type'              =>  'text'
+    ));
+
+    $wp_customize->add_setting('educare_show_topbar_right_btn_link_settings', array(
+        'capability'        => 'edit_theme_options',
+        'transport'         => 'refresh',
+        'type'              => 'theme_mod',
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    $wp_customize->add_control('educare_show_topbar_right_btn_link_ctrl', array(
+        'label'             =>  __('Button Label', 'educare'),
+        'section'           =>  'educare_topbar',
+        'settings'          =>  'educare_show_topbar_right_btn_link_settings',
+        'type'              =>  'text'
     ));
