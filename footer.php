8956f2cbd0a54f12105e4645a3679ec1cb5eb93f
saidmsaif <saidmsaiff@gmail.com>
2022-02-21 11:56:00 +0600

    add footer credit

--- footer.php
+++ footer.php
@@ -1,62 +1,15 @@
     <!-- footer-start  -->
     <footer class="footer">
-        <div class="main-container">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-3 col-md-6 mt-4">
-                        <div class="footer-nav">
-                            <a class="navbar-brand p-0" href="#"><span class="logo-text-1">twin</span><span class="logo-text-2">kle </span></a>
-                            <p>329 Queensberry Street, North Melbourne VIC 3051, Australia.</p>
-                            <p>+0999999</p>
-                            <p>demo@gmail.com</p> 
-                        </div>
-                        <div class="footer-social-icon mt-4">
-                            <a href="#"><i class="fab fa-facebook"></i></a>
-                            <a href="#"><i class="fab fa-twitter"></i></a>
-                            <a href="#"><i class="fab fa-linkedin"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-md-6 mt-4">
-                        <h2 class="footer-title">Our School</h2>
-                        <ul class="footer-list-items">
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Notice</a></li>
-                            <li><a href="#">Courses</a></li>
-                            <li><a href="#">Accademics</a></li>
-                            <li><a href="#">Teachers</a></li>
-                            <li><a href="#">Events</a></li>
-                            <li><a href="#">Blog</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-xl-3 col-md-6 mt-4">
-                        <h2 class="footer-title">twinkle Couress</h2>
-                        <ul class="footer-list-items">
-                            <li><a href="#">Starting a Business</a></li>
-                            <li><a href="#">Android Development</a></li>
-                            <li><a href="#">PHP Development</a></li>
-                            <li><a href="#">Business & Management </a></li>
-                            <li><a href="#">Physics</a></li>
-                            <li><a href="#">Business English</a></li>
-                            <li><a href="#">Computer science</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-xl-3 col-md-6 mt-4">
-                        <h2 class="footer-title">Important Link</h2>
-                        <ul class="footer-list-items">
-                            <li><a href="#">Education Board</a></li>
-                            <li><a href="#">Education Board Result</a></li>
-                            <li><a href="#">Ministry of Education</a></li>
-                            <li><a href="#">DG Website</a></li>
-                            <li><a href="#">Banbeis</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="row">
-            <div class="col-md-12 mt-5">
+            <div class="col-md-12">
                 <div class="footer-massage">
-                    <p>Copyright &copy; 2022 twinkle. All Right Reserved.</p>
+                    <?php
+                        if(get_theme_mod('educare_footer_credit_settings')) {
+                            echo esc_html( get_theme_mod('educare_footer_credit_settings'));
+                        } else {
+                            echo '@ EduCare | Developed by <a href="https://mamurjor.com/">Mamurjor IT</a> | 2022';
+                        }
+                    ?>
                 </div>
             </div>
         </div>
