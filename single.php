05bca6747832df0e9526429ba1546080d1661e7f
saidmsaif <saidmsaiff@gmail.com>
2022-02-21 13:51:16 +0600

    add single page

---
+++ single.php
@@ -0,0 +1,41 @@
+<?php
+    get_header();
+?>
+
+<main>
+    <!-- single-event-details-start -->
+    <section>
+        <!-- single-coureses-start-second  -->
+        <div class="main-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-8">
+                        <div class="mt-2 event-document">
+                            <?php
+                                if(has_post_thumbnail()) {
+                                    the_post_thumbnail( 'large', array('class'=>'w-100 mb-4 img-fluid h-auto') );
+                                }
+                            ?>
+                            <h2 class="fw-bold">
+                                <?php the_title(); ?>
+                            </h2>
+                            <?php
+                                the_content();
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-xl-4">
+                        <?php
+                            echo the_field('educare_post_meta');
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- single-coureses-start-second-close  -->
+    </section>
+</main>
+
+
+<?php
+    get_footer();
