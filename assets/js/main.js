05e8e93c3731b3e6abbf09293923476e5dd75196
saidmsaif <saidmsaiff@gmail.com>
2022-02-24 10:05:44 +0600

    remove unneccessary code

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,29 +6,18 @@
 // back-to-top 
 myButton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {educare_scrollFunction();};
 
-function scrollFunction() {
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myButton.style.display = "block";
-} else {
-    myButton.style.display = "none";
-}
+function educare_scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      myButton.style.display = "block";
+  } else {
+      myButton.style.display = "none";
+  }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
-
-// Dropdown Menu
-
-// document.addEventListener('click', e => {
-//     if (e.target.classList.contains("dropdown-toggle")) {
-//         document.querySelector(".dropdown-menu").classList.toggle('d-block')
-        
-//         console.log(e.target.firstChild.nodeName);
-// 	}
-// })
