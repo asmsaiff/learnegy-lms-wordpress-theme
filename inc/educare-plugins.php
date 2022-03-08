dc3f9356bbf4ce5a8838d6492ee9b9bf06ce5a41
saidmsaif <saidmsaiff@gmail.com>
2022-03-08 19:50:36 +0600

    add educare core plugin to this theme

--- inc/educare-plugins.php
+++ inc/educare-plugins.php
@@ -61,6 +61,17 @@ function educare_register_required_plugins() {
 
 		// This is an example of how to include a plugin bundled with a theme.
 		array(
+			'name'               => 'EduCare Core', // The plugin name.
+			'slug'               => 'educare-core', // The plugin slug (typically the folder name).
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '1.0.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+
+		array(
 			'name'               => 'Advanced Custom Fields', // The plugin name.
 			'slug'               => 'advanced-custom-fields', // The plugin slug (typically the folder name).
 			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
